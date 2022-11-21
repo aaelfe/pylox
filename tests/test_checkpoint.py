ae620932bc21f5afaaec959730d6f08bc093a179
Allan Elfe <aaelfe@crimson.ua.edu>
2022-11-21 19:26:17 +0000

    Updated tests for checkpoint (finished with chapter 9)

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -4,12 +4,85 @@ from io import StringIO
 
 stdout_fileno = sys.stdout
 
-#This test executes the run function in lox.py with sample Lox code from the file test.lox
-#Passes if tokens printed by scanner match the result stored in the testdotlox variable
-def test_run_testdotlox():
+loopsOutput = """1
+2
+3
+4
+1
+2
+3
+4
+"""
+
+scopeOutput = """inner a
+outer b
+global c
+outer a
+outer b
+global c
+global a
+global b
+global c
+"""
+
+ifElseOutput = """Variable a equals y
+Variable a doesn't equal z
+Variable a equals x
+"""
+
+expressionsOutput = """39
+false
+true
+"""
+
+testOutput = """Not three strikes.
+x
+Not three strikes.
+xx
+Three strikes!
+xxx
+Not three strikes.
+xxxx
+"""
+
+#This test runs examples/loops.lox through the interpreter
+#Passes if the stdout equals the content of the loopsOutput variable
+def test_loops():
     sys.stdout = myOutput = StringIO()
     interpreter = Lox()
     interpreter.runFile('examples/loops.lox')
-    assert myOutput.getvalue() == "yo"
-    
+    assert myOutput.getvalue() == loopsOutput
+
+#This test runs examples/scope.lox through the interpreter
+#Passes if the stdout equals the content of the scopeOutput variable
+def test_scope():
+    sys.stdout = myOutput = StringIO()
+    interpreter = Lox()
+    interpreter.runFile('examples/scope.lox')
+    assert myOutput.getvalue() == scopeOutput
+
+#This test runs examples/ifElse.lox through the interpreter
+#Passes if the stdout equals the content of the ifElseOutput variable
+def test_if_else():
+    sys.stdout = myOutput = StringIO()
+    interpreter = Lox()
+    interpreter.runFile('examples/ifElse.lox')
+    assert myOutput.getvalue() == ifElseOutput
+
+#This test runs examples/expressions.lox through the interpreter
+#Passes if the stdout equals the content of the expressionsOutput variable
+def test_expressions():
+    sys.stdout = myOutput = StringIO()
+    interpreter = Lox()
+    interpreter.runFile('examples/expressions.lox')
+    assert myOutput.getvalue() == expressionsOutput
+
+#This test runs examples/test.lox through the interpreter
+#Passes if the stdout equals the content of the testOutput variable
+def test_test():
+    sys.stdout = myOutput = StringIO()
+    interpreter = Lox()
+    interpreter.runFile('examples/test.lox')
+    assert myOutput.getvalue() == testOutput
+
 sys.stdout = stdout_fileno
